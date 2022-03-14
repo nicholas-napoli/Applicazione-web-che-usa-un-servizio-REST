9053abf8062a72f46b8238fe103fda27794eb4e2
Nicholas Napoli <nicholas.napoli@marconi.loc>
2022-03-14 08:38:47 +0100

    aggiunta di api key

---
+++ app.js
@@ -0,0 +1,7 @@
+let auth = 'ae095ba3a39c4c0d8ab8245e523574af'
+
+fetch(`https://api.spoonacular.com/recipes/complexSearch?&apiKey=ae095ba3a39c4c0d8ab8245e523574af`).then(res=>{
+    return res.json()
+}).then(data =>{
+
+})
